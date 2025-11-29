96500ea90ff7693fb83e11238823a65d521c7ece
JimmyGAR <jimmy.garnier1110@gmail.com>
2025-11-29 18:38:53 +0100

    Mise en place de l'affiche des étoiles en fonction des notes attribué au client (en brut/manuel) → automatique en cours

--- newMVC/templates/userProfil.php
+++ newMVC/templates/userProfil.php
@@ -1,22 +1,29 @@
 <?php
 $title = "Page d'utilisateur";
 $style = "templates/style/style.css";
-?>
+$optional_style1 = "templates/style/userProfil.css"
+    ?>
 
 <?php ob_start(); ?>
-    <header>
-        <?php include('preset/header.php'); ?>
-    </header>
+<header>
+    <?php include('preset/header.php'); ?>
+</header>
 
-    <main>
-        
-        <?php
-        echo "<p class='PsuedoClient'>" . $u['firstname'] . " " . $u['name'] . "</p>";
-        ?>
-    </main>
-        <footer>
-            <?php include('preset/footer.php'); ?>
-        </footer>
+<main>
+
+    <?php
+    echo "<p class='PsuedoClient'>" . $u['firstname'] . " " . $u['name'] . "</p>";
+    ?>
+    <div id="stars">
+        <img src="./templates/images/stars_design.png">
+        <div id="c1"></div>
+        <div id="c2"></div>
+    </div>
+</main>
+
+<footer>
+    <?php include('preset/footer.php'); ?>
+</footer>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('preset/layout.php'); ?>
