b7e281afbd3314cea190351126fd7f6954150523
JimmyGAR <jimmy.garnier1110@gmail.com>
2025-12-01 16:43:07 +0100

    Ajout du style pour les pages Inscription et Connection

--- newMVC/templates/connection.php
+++ newMVC/templates/connection.php
@@ -1,46 +1,44 @@
 <?php
 $title = "Page de connexion";
-$style = "templates/style/style.css";
+$style = "templates/style/Accueil.css";
 ?>
 
 <?php ob_start(); ?>
-    <style>
-        footer {
-            position: absolute;
-            bottom: 0;
-        }
-    </style>
+<style>
+    footer {
+        position: absolute;
+        bottom: 0;
+    }
+</style>
 
-    <header>
-        <?php include('preset/header.php'); ?>
-    </header>
 
-    <main>
-        <div>
-            <hr>
-            <h1>Connectez-vous à votre compte.</h1>
-            <form action="index.php?action=userConnection" method="post" id="connexion">
-                <div>
-                    <h2>Email</h2>
-                    <input type="text" name="email" placeholder="Email">
-                </div>
-                <div>
-                    <h2>Mot de passe</h2>
-                    <input type="password" name="password" placeholder="Mot de passe">
-                </div>
+<?php include('preset/header.php'); ?>
 
-                <br>
-                <input type="submit" value="Connexion">
-            </form>
-            <p>Vous n'avez pas encore de compte, inscrivez-vous dès maintenant en cliquant <a
-                    href="index.php?action=inscription">ici</a></p>
-            <hr>
-        </div>
-    </main>
+<main>
+    <section class="connection">
+        <hr>
+        <h1>Connectez-vous à votre compte.</h1>
+        <form action="index.php?action=userConnection" method="post" id="connexion">
+            <div>
+                <label for="connection-email">Email</label>
+                <input type="text" id="connection-email" name="email" placeholder="Email">
+            </div>
+            <div>
+                <label for="connection-password">Mot de passe</label>
+                <input type="password" id="connection-password" name="password" placeholder="Mot de passe">
+            </div>
 
-    <footer>
-        <?php include('preset/footer.php'); ?>
-    </footer>
+            <br>
+            <input type="submit" value="Connexion">
+        </form>
+        <p>Vous n'avez pas encore de compte, inscrivez-vous dès maintenant en cliquant <a
+                href="index.php?action=inscription">ici</a></p>
+        <hr>
+    </section>
+</main>
+
+<?php include('preset/footer.php'); ?>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('preset/layout.php'); ?>
--- newMVC/templates/inscription.php
+++ newMVC/templates/inscription.php
@@ -1,61 +1,59 @@
 <?php
 $title = "Page d'inscription'";
-$style = "templates/style/style.css";
+$style = "templates/style/Accueil.css";
 ?>
 
 <?php ob_start(); ?>
-    <header>
-        <?php include('preset/header.php'); ?>
-    </header>
 
-    <main>
-        <div>
-            <hr>
-            <h1>Inscrivez-vous.</h1>
-            <form action="index.php?action=userInscription" method="post" id="connexion">
-                <div>
-                    <h2>Nom</h2>
-                    <input type="text" name="name" placeholder="Nom">
-                </div>
-                <div>
-                    <h2>Prenom</h2>
-                    <input type="text" name="firstname" placeholder="Prenom">
-                </div>
-                <div>
-                    <h2>Date de naissance</h2>
-                    <input type="date" name="birth_date">
-                </div>
-                <div>
-                    <h2>Adresse</h2>
-                    <input type="text" name="address" placeholder="Adresse">
-                </div>
-                <div>
-                    <h2>Ville</h2>
-                    <input type="text" name="city" placeholder="Ville">
-                </div>
-                <div>
-                    <h2>Code postal</h2>
-                    <input type="number" name="postal_code" placeholder="Code postal">
-                </div>
-                <div>
-                    <h2>Email</h2>
-                    <input type="text" name="email" placeholder="Email">
-                </div>
-                <div>
-                    <h2>Mot de passe</h2>
-                    <input type="password" name="password" placeholder="Mot de passe">
-                </div>
+<?php include('preset/header.php'); ?>
 
-                <br>
-                <input type="submit" value="Connexion">
-            </form>
-            <hr>
-        </div>
-    </main>
+<main>
+    <section class="inscription">
+        <hr>
+        <h1>Inscrivez-vous.</h1>
+        <form action="index.php?action=userInscription" method="post" id="connexion">
+            <div>
+                <label for="inscription-name">Nom</label>
+                <input type="text" id="inscription-name" name="name" placeholder="Nom">
+            </div>
+            <div>
+                <label for="inscription-firstname">Prenom</label>
+                <input type="text" id="inscription-firstname" name="firstname" placeholder="Prenom">
+            </div>
+            <div>
+                <label for="inscription-birtdate">Date de naissance</label>
+                <input type="date" id="inscription-birtdate" name="birth_date">
+            </div>
+            <div>
+                <label for="inscription-address">Adresse</label>
+                <input type="text" id="inscription-address" name="address" placeholder="Adresse">
+            </div>
+            <div>
+                <label for="inscription-city">Ville</label>
+                <input type="text" id="inscription-city" name="city" placeholder="Ville">
+            </div>
+            <div>
+                <label for="inscription-postalCode">Code postal</label>
+                <input type="number" id="inscription-postalCode" name="postal_code" placeholder="Code postal">
+            </div>
+            <div>
+                <label for="inscription-email">Email</label>
+                <input type="text" id="inscription-email" name="email" placeholder="Email">
+            </div>
+            <div>
+                <label for="inscription-password">Mot de passe</label>
+                <input type="password" id="inscription-password" name="password" placeholder="Mot de passe">
+            </div>
 
-    <footer>
-        <?php include('preset/footer.php'); ?>
-    </footer>
+            <br>
+            <input type="submit" value="Connexion">
+        </form>
+        <hr>
+    </section>
+</main>
+
+<?php include('preset/footer.php'); ?>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('preset/layout.php');
