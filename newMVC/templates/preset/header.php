c5be4b3009687ebd144c7b5d569c54c8426e1f09
Kyno3146 <kyllian_riviere@etu.u-bourgogne.fr>
2025-12-10 16:24:58 +0100

    Barre de header

--- newMVC/templates/preset/header.php
+++ newMVC/templates/preset/header.php
@@ -1,15 +1,46 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+
+
 <header>
-    <nav>
-        <ul>
-            <li><a id="btn_achat" href="index.php?action=buy">Acheter</a></li>
-            <?php if (isset($_SESSION['user'])) { ?>
-                <li><a id="btn_vente" href="index.php?action=sell">Vendre</a></li>
-                <li><a id="btn_client" href="index.php?action=user">Client</a></li>
-            <?php } ?>
-        </ul>
-        <a href="index.php">
-            <img>
-        </a>
-        <a id="btn_connexion" href="index.php?action=connection">Connexion</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
+        <div class="container-fluid">
+            
+            <a class="navbar-brand" href="index.php">
+                <img src="votre-logo.png" alt="Logo" style="height: 30px;">
+            </a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+                
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a id="btn_achat" class="nav-link" href="index.php?action=buy">Acheter</a>
+                    </li>
+                    
+                    <?php if (isset($_SESSION['user'])) { ?>
+                        <li class="nav-item">
+                            <a id="btn_vente" class="nav-link" href="index.php?action=sell">Vendre</a>
+                        </li>
+                        <li class="nav-item">
+                            <a id="btn_client" class="nav-link" href="index.php?action=user">Client</a>
+                        </li>
+                    <?php } ?>
+                </ul>
+                
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a id="btn_connexion" class="nav-link" href="index.php?action=connection">Connexion</a>
+                    </li>
+                </ul>
+                
+            </div>
+            
+        </div>
     </nav>
 </header>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
