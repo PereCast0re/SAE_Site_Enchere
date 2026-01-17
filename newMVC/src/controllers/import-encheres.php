a61b57161297db834482450ff9a19ca3430fe573
Kyno3146 <kyllian_riviere@etu.u-bourgogne.fr>
2026-01-17 13:26:10 +0100

    Recherche meilisearch

--- newMVC/src/controllers/import-encheres.php
+++ newMVC/src/controllers/import-encheres.php
@@ -1,18 +1,26 @@
 <?php
-
-require __DIR__.'/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
 use Meilisearch\Client;
 
 $client = new Client('http://127.0.0.1:7700', 'CLE_TEST_SAE_SITE');
 
-$pdo = new PDO('mysql:host=localhost;dbname=auction_site', 'root', '');     
 
-$statement = $pdo->query("SELECT id_product AS id, title, description, start_date, end_date, reserve_price, start_price, status FROM product");
-$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+try {
+    $client->createIndex('search', [
+        'primaryKey' => 'id'
+    ]);
+} catch (Exception $e) {
+}
 
-$index = $client->createIndex('products', ['primaryKey' => 'id']);
 
-$index = $client->index('products');
+$index = $client->index('search');
 
-$index->addDocuments($products);
+$index->updateSettings([
+    'searchableAttributes' => ['title', 'category_name', 'celebrity_name'],
+    'filterableAttributes' => ['type','category_id','celebrity_id'],
+    'displayedAttributes' => ['id','type','title','product_id','category_id','celebrity_id','category_name','celebrity_name']
+]);
+
+
+echo "Index 'search' créé et configuré avec succès";
