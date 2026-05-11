a3ec9c6f327b11b27056f0e825cf36744e64c7a6
JimmyGAR <jimmy.garnier1110@gmail.com>
2026-05-11 22:43:35 +0200

    Remise en place et vérification en cours

--- newMVC/src/Lib/DatabaseConnection.php
+++ newMVC/src/Lib/DatabaseConnection.php
@@ -7,11 +7,12 @@ use PDO;
 class DatabaseConnection
 {
     private static ?PDO $pdo = null;
+
     public static function getConnection(): PDO
     {
         if (self::$pdo === null) {
-            self::$pdo = new PDO('mysql:host=db;dbname=auction_site;
-                charset=utf8', 'root', 'root_password');
+            self::$pdo = new PDO('mysql:host=127.0.0.1;dbname=auction_site;
+                charset=utf8', 'root', '');
             /// Permet de lancer une exception si le pdo a une problème (requête SQL, connection, ...)
             self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             ///
